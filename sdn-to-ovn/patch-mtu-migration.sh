5016d84f7218145569e3bee09bc9d7a10c0e9bd1
Ross Brattain <rbrattai@redhat.com>
2025-04-17 13:39:11 -0400

    add patch-mtu-migration script

---
+++ sdn-to-ovn/patch-mtu-migration.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+for f in $(oc get pods -n openshift-machine-config-operator \
+  -l k8s-app=machine-config-daemon \
+  --no-headers \
+  -o custom-columns=N:.metadata.name); do
+
+  echo "$f"
+
+  oc exec -n openshift-machine-config-operator "$f" -c machine-config-daemon -- \
+    chroot /rootfs bash -c "mkdir -p /etc/systemd/system/mtu-migration.service.d && \
+      printf '%s\n' '[Unit]' 'After=wait-for-primary-ip.service' | \
+        tee /etc/systemd/system/mtu-migration.service.d/override.conf && \
+      systemctl daemon-reload"
+done
