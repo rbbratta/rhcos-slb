d757d20eb41799e89ed0607301a12df6a1548fd9
Ross Brattain <rbrattai@redhat.com>
2025-06-16 11:03:08 -0400

    add script to convert SDN nmtate to OVN

---
+++ sdn-to-ovn/convert-nmstate.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+set -x
+
+PTH=${0%/*}
+
+
+ROLE=$1
+shift
+IN=$1
+
+HOSTNAME=$(yq '.hostname.running' "${IN}" | cut -d . -f 1)
+MTU=$(yq '.interfaces[] | select(.description == "primary") |  .mtu' "${IN}")
+PRIMARY_MAC=$(yq '.interfaces[] | select(.description == "primary") |   .["mac-address"]' "${IN}")
+PRIMARY=$(yq '.interfaces[] | select(.description == "primary") | .name' "${IN}")
+SECONDARY=$(yq '.interfaces[] | select(.description == "secondary") |  .name' "${IN}")
+EXTRA=$(yq '.interfaces[] | select(.ipv4.enabled == false and .type == "ethernet" and .description == null) | .name'  "${IN}")
+
+
+${PTH}/make-br-ex-nmstate.sh ${HOSTNAME} ${ROLE} ${MTU} ${PRIMARY_MAC} ${PRIMARY} ${SECONDARY} ${EXTRA}
